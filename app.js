f24e468ff4280506263cbc6d6a14586c1f6d1200
wassim-triki <83872942+wassim-triki@users.noreply.github.com>
2021-06-26 04:07:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,175 +1,146 @@
-const start=document.querySelector("#start");
-const GRID=document.querySelector(".grid");
+const start = document.querySelector("#start");
+const GRID = document.querySelector(".grid");
+const resetBtn = document.querySelector(".reset");
+const pauseBtn = document.querySelector(".pause");
+const count = document.querySelector(".count");
+
+let genCount = 0;
+
+let cellSize = Number(document.querySelector("#cell-size").value);
+const gridW = GRID.offsetWidth;
+const gridH = GRID.offsetHeight;
+let cols = Math.floor(gridW / cellSize);
+let rows = Math.floor(gridH / cellSize);
 
 const randInt = (max, min) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-const randColor=(col1,col2)=>{
-  if(col1 && col2){
-    return [col1,col2][randInt(0,2)];
+const randColor = (col1, col2) => {
+  if (col1 && col2) {
+    return [col1, col2][randInt(0, 2)];
   }
-  return`rgb(${randInt(0,256)},${randInt(0,256)},${randInt(0,256)})`;
-}
+  return `rgb(${randInt(0, 256)},${randInt(0, 256)},${randInt(0, 256)})`;
+};
 
-
-
-
-const cellSize=Number(prompt("cell size: "));
-const gridW=GRID.offsetWidth;
-const gridH=GRID.offsetHeight;
-const cols=Math.floor(gridW/cellSize);
-const rows=Math.floor(gridH/cellSize);
-
-
-class Cell{
-  constructor(size){
-    this.size=size;
-    this.isAlive=false;
+class Cell {
+  constructor(size) {
+    this.size = size;
+    this.isAlive = false;
     this.html;
-    this.liveNeighbours=[];
+    this.liveNeighbours;
   }
 
-  die(){
-    this.isAlive=false;
-    this.html.style.background="#fff";
+  die() {
+    this.isAlive = false;
+    this.html.style.background = "#fff";
   }
-  live(){
-    this.isAlive=true;
-    this.html.style.background="#000";
+  live() {
+    this.isAlive = true;
+    this.html.style.background = "#000";
   }
 
-  getLiveNeighbours(){
+  getLiveNeighbours() {
     return this.liveNeighbours.length;
   }
-  makeCell(){
-    const cell=document.createElement("div");
-    cell.style.display="inline-block";
-    cell.style.outline="1px solid black";
-    cell.style.width=`${this.size}px`;
-    cell.style.height=`${this.size}px`;
-    cell.style.background=this.isAlive?"#000":"#fff";
+
+  makeCell() {
+    const cell = document.createElement("div");
+    cell.style.display = "inline-block";
+    cell.style.outline = "1px solid #aaa";
+    cell.style.width = `${this.size}px`;
+    cell.style.height = `${this.size}px`;
+    cell.style.background = this.isAlive ? "#28eb96" : "#37303d";
     return cell;
-  }
-
-
-  highlight(){
-    this.html.style.background="red";
-  }
-
-  highlightliveNeighbours(){
-    const col=randColor();
-    for(let neighbour of this.liveNeighbours){
-      if(neighbour){
-        neighbour.html.style.background=col;
-      }
-    }
-  }
-
-  clearHighlight(){
-    for(let neighbour of this.liveNeighbours){
-      if(neighbour){
-        neighbour.html.style.background=randColor("#000","#fff");
-      }
-    }
   }
 }
 
-const initGridCells=()=>{
-  const grid=[];
-  for(let i=0;i<rows;i++){
-    const row=[];
-    for(let j=0;j<cols;j++){
-      const cell=new Cell(cellSize);
-      cell.isAlive=Boolean(randInt(0,2));
+const initGridCells = () => {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      const cell = new Cell(cellSize);
+      cell.isAlive = Boolean(randInt(0, 2));
       row.push(cell);
     }
     grid.push(row);
   }
   return grid;
-}
+};
 
-
-const drawGrid=(grid)=>{
-  GRID.innerHTML="";
-  for(let i=0;i<rows;i++){
-    const rowDiv=document.createElement("div");
-    rowDiv.style.display="flex";
-    for(let j=0; j<cols;j++){
-      const cell=grid[i][j];
-      const cellDiv=cell.makeCell();
-      cell.html=cellDiv;
+const drawGrid = (grid) => {
+  GRID.innerHTML = "";
+  for (let i = 0; i < rows; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.style.display = "flex";
+    for (let j = 0; j < cols; j++) {
+      const cell = grid[i][j];
+      const cellDiv = cell.makeCell();
+      cell.html = cellDiv;
       rowDiv.appendChild(cellDiv);
     }
     GRID.appendChild(rowDiv);
   }
-}
+};
 
-const getemptyCells=()=>{
-  const grid=[];
-  for(let i=0;i<rows;i++){
-    const row=[];
-    for(let j=0;j<cols;j++){
+const getemptyCells = () => {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
       row.push(null);
     }
     grid.push(row);
   }
-  return grid
-}
+  return grid;
+};
 
-const getAliveNeighboursArray=(arr,i,j)=>{
-  const liveNeighbours=[];
-  for(let k=-1;k<2;k++){
-    if(arr[i+k]){
-      for(let l=-1;l<2;l++){
-        if(arr[i+k][j+l] && arr[i+k][j+l].isAlive && arr[i+k][j+l]!=arr[i][j]){
-          liveNeighbours.push( arr[i+k][j+l]);
+const getAliveNeighboursArray = (arr, i, j) => {
+  let liveNeighbours = 0;
+  for (let k = -1; k < 2; k++) {
+    if (arr[i + k]) {
+      for (let l = -1; l < 2; l++) {
+        if (
+          arr[i + k][j + l] &&
+          arr[i + k][j + l].isAlive &&
+          arr[i + k][j + l] != arr[i][j]
+        ) {
+          liveNeighbours++;
         }
       }
     }
   }
   return liveNeighbours;
-}
+};
 
-const setAliveNeighbours=(arr)=>{
-  for(let i=0;i<rows;i++){
-    for(let j=0;j<cols;j++){
-      const cell=arr[i][j];
-      cell.liveNeighbours=getAliveNeighboursArray(arr,i,j);
+const setAliveNeighbours = (arr) => {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const cell = arr[i][j];
+      cell.liveNeighbours = getAliveNeighboursArray(arr, i, j);
     }
   }
-}
+};
 
-
-let cells1=initGridCells();
-setAliveNeighbours(cells1);
-drawGrid(cells1);
-
-
-
-
-
-const getNextGenrationArray=(oldArr)=>{
-  const newArr=[];
-  for(let i=0;i<rows;i++){
-    const row=[];
-    for(let j=0; j<cols;j++){
-      const oldCell=oldArr[i][j];
-      let newCell=new Cell(cellSize);
-      const neighbours=oldCell.getLiveNeighbours();
-      if(oldCell.isAlive){
-        if(neighbours<2 || neighbours>3){
-          //dies
-          newCell.isAlive=false;
+const getNextGenrationArray = (oldArr) => {
+  const newArr = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      const oldCell = oldArr[i][j];
+      let newCell = new Cell(cellSize);
+      const neighbours = oldCell.liveNeighbours;
+      if (oldCell.isAlive) {
+        if (neighbours < 2 || neighbours > 3) {
+          newCell.isAlive = false;
         }
-        if(neighbours==2 || neighbours==3){
-          //lives
-          newCell.isAlive=true;
+        if (neighbours == 2 || neighbours == 3) {
+          newCell.isAlive = true;
         }
-      }else{
-        if(neighbours==3){
-          //becomes Alive
-          newCell.isAlive=true;
+      } else {
+        if (neighbours == 3) {
+          newCell.isAlive = true;
         }
       }
       row.push(newCell);
@@ -177,39 +148,54 @@ const getNextGenrationArray=(oldArr)=>{
     newArr.push(row);
   }
   return newArr;
-}
+};
 
-
-
-const equal=(grid1,grid2)=>{
-  for(let i=0;i<rows;i++){
-    for(let j=0;j<cols;j++){
-      if(grid1[i][j]!=grid2[i][j]){
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-
-const gameOfLife=()=>{
-  let nextGen=getNextGenrationArray(cells1);
+const drawNextGen = () => {
+  count.textContent = `Gen Count: ${genCount++}`;
+  let nextGen = getNextGenrationArray(cells1);
   setAliveNeighbours(nextGen);
   drawGrid(nextGen);
-  cells1=nextGen;
-}
+  cells1 = nextGen;
+};
 
+let cells1 = initGridCells();
+const setup = () => {
+  count.textContent = `Gen Count: ${genCount}`;
+  cellSize = Number(document.querySelector("#cell-size").value);
+  const gridW = GRID.offsetWidth;
+  const gridH = GRID.offsetHeight;
+  cols = Math.floor(gridW / cellSize);
+  rows = Math.floor(gridH / cellSize);
+};
 
+const reset = () => {
+  genCount = 0;
+  setup();
+  cells1 = initGridCells();
+  displayGrid();
+};
 
-let interval=setInterval(gameOfLife, 50);
+const displayGrid = () => {
+  setAliveNeighbours(cells1);
+  drawGrid(cells1);
+};
 
-document.addEventListener("click",()=>{
-  clearInterval(interval);
-})
+setup();
+displayGrid();
 
+let isPaused = true;
+pauseBtn.classList.replace("pause", "resume");
+let interval;
 
-
-
-
-
+resetBtn.addEventListener("click", reset);
+pauseBtn.addEventListener("click", () => {
+  if (isPaused) {
+    interval = setInterval(drawNextGen, 50);
+    isPaused = false;
+    pauseBtn.classList.replace("resume", "pause");
+  } else {
+    clearInterval(interval);
+    pauseBtn.classList.replace("pause", "resume");
+    isPaused = true;
+  }
+});
